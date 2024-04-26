0df40ed104e22e3b75b59d10b5422dfc52283bc9
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-04-26 19:00:57 +0800

    router implemented

---
+++ src/pages/NoPage.js
@@ -0,0 +1,9 @@
+// export default function NoPage({errorCode, message}) {
+export default function NoPage() {
+
+    return (
+        <div>
+            Erorr 404: Not Found!
+        </div>
+    )
+}
