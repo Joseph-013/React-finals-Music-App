f87841d79339e8a875a6c83f98061d9d5e9a5820
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-04-30 12:49:57 +0800

    tiles

--- src/components/TileGridHorizontal.js
+++ src/components/TileGridHorizontal.js
@@ -2,7 +2,7 @@ function TileGridHorizontal({ children, title }) {
     return (
         <div className="w-full flex flex-col font-poppins space-y-3">
             <span className="w-full text-left text-lg md:text-2xl font-bold">{title}</span>
-            <ul className="max-w-full h-[14.5rem] grid grid-rows-1 grid-flow-col overflow-x-auto gap-x-10 justify-start font-poppins">
+            <ul className="max-w-full grid grid-rows-1 grid-flow-col overflow-x-auto gap-x-10 justify-start font-poppins">
                 {children}
             </ul>
         </div>
--- src/components/TileRounded.js
+++ src/components/TileRounded.js
@@ -1,6 +1,6 @@
 function TileRounded(data) {
     return (
-        <li className="w-44 h-full flex flex-col justify-between text-left">
+        <li className="w-44 h-[14.5rem] flex flex-col justify-between text-left">
             <img src={data.src} className="w-full rounded-full" height="auto" alt={data.name + ` ` + data.type} />
             <div className="flex flex-col w-full">
                 <span className="truncate text-base">{data.title}</span>
