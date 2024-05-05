50c5504635abc385dc279653b16e80defe13c2c6
Redvinz <arvinalkuino123@gmail.com>
2024-05-05 17:45:20 +0800

    buttons

--- src/components/SectionContainer.js
+++ src/components/SectionContainer.js
@@ -1,10 +1,10 @@
 function SectionContainer({ children, title }) {
-    return (
-        <div className="flex flex-col space-y-3">
-            <span className="w-full text-left text-lg md:text-2xl font-bold">{title}</span>
-            {children}
-        </div>
-    )
+  return (
+    <div className="flex flex-col space-y-3">
+      <span className="w-full text-left text-lg md:text-2xl font-bold">{title}</span>
+      {children}
+    </div>
+  );
 }
 
-export default SectionContainer
+export default SectionContainer;
--- src/components/TrackItem.js
+++ src/components/TrackItem.js
@@ -1,39 +1,89 @@
-import visualizerGif from './icons/visualizerGif.gif';
-import { useState } from 'react';
-import IconPlayFilled from './icons/IconPlayFilled';
+import visualizerGif from "./icons/visualizerGif.gif";
+import { useState } from "react";
+import IconPlayFilled from "./icons/IconPlayFilled";
 
 function TrackItem(track) {
-    const [hovered, setHovered] = useState(false);
-    let widthCount = 0;
-    let width;
-    if (track.duration) ++widthCount;
-    if (track.genre) ++widthCount;
-    switch (widthCount) {
-        case 2: { width = 'w-3/5'; break; }
-        case 1: { width = 'w-4/5'; break; }
-        default: width = 'w-full';
+  const [hovered, setHovered] = useState(false);
+  let widthCount = 0;
+  let width;
+  if (track.duration) ++widthCount;
+  if (track.genre) ++widthCount;
+  switch (widthCount) {
+    case 2: {
+      width = "w-3/5";
+      break;
     }
+    case 1: {
+      width = "w-4/5";
+      break;
+    }
+    default:
+      width = "w-full";
+  }
 
-    return (
-        <div className={`w-full h-full flex items-center justify-between text-left space-x-2 cursor-pointer hover:bg-cyan-700 p-2 -mx-2 rounded-lg ` + (!track.playing || 'border border-cyan-600')}
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-        >
-            <div className={`h-full flex items-center space-x-3 ` + width}>
-                <div className="relative w-fit">
-                    {(!track.playing) || <img src={visualizerGif} alt='Play' className='opacity-40 absolute' />}
-                    <IconPlayFilled size="25" className={`absolute justify-center items-center h-full w-full p-2 bg-gray-900 opacity-60 ` + (hovered && !track.playing && !track.playerComponent ? `flex` : `hidden`)} />
-                    <img src={track.cover} alt={track.title} className="size-12" />
-                </div>
-                <div className="flex-1 flex flex-col truncate font-poppins w-fit">
-                    <span className="text-sm truncate tracking-normal">{track.title}</span>
-                    <span className="text-sm text-slate-400 truncate">{track.artist}</span>
-                </div>
-            </div>
-            {(!track.genre) || <span className="h-full w-fit flex items-center text-center max-w-24 text-slate-400 line-clamp-2">{track.genre}</span>}
-            {(!track.duration) || <span className="h-full w-fit flex items-center text-slate-400">{track.duration}</span>}
+  return (
+    <div
+      className={
+        `w-full h-full flex items-center justify-between text-left space-x-2 cursor-pointer hover:bg-cyan-700 p-2 -mx-2 rounded-lg ` +
+        (!track.playing || "border border-cyan-600")
+      }
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <div className={`h-full flex items-center space-x-3 ` + width}>
+        <div className="relative w-fit">
+          {!track.playing || (
+            <img src={visualizerGif} alt="Play" className="opacity-40 absolute" />
+          )}
+          <IconPlayFilled
+            size="25"
+            className={
+              `absolute justify-center items-center h-full w-full p-2 bg-gray-900 opacity-60 ` +
+              (hovered && !track.playing && !track.playerComponent
+                ? `flex`
+                : `hidden`)
+            }
+          />
+          <img src={track.cover} alt={track.title} className="size-12" />
         </div>
-    )
+        <div className="flex-1 flex flex-col truncate font-poppins w-fit">
+          <span className="text-sm truncate tracking-normal">{track.title}</span>
+          <span className="text-sm text-slate-400 truncate">{track.artist}</span>
+        </div>
+      </div>
+      {!track.genre || (
+        <span className="h-full w-fit flex items-center text-center max-w-24 text-slate-400 line-clamp-2">
+          {track.genre}
+        </span>
+      )}
+      <button class="flex items-center justify-center size-10 hover:bg-cyan-700 hover:text-white rounded-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="icon icon-tabler icon-tabler-heart"
+          width="25"
+          height="25"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          opacity={track.liked ? "1" : "0.75"}
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"></path>
+        </svg>
+      </button>{" "}
+      {!track.duration || (
+        <span className="h-full w-fit flex items-center text-slate-400">
+          {track.duration}
+        </span>
+      )}
+      <button class="flex items-center justify-center size-10 hover:bg-cyan-700 hover:text-white rounded-full">
+        ...
+      </button>{" "}
+    </div>
+  );
 }
 
-export default TrackItem
+export default TrackItem;
