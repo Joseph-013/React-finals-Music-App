6f9f31a656dac85dc5bf67d25df8f271a5775678
Redvinz <arvinalkuino123@gmail.com>
2024-05-05 23:09:20 +0800

    asd

--- src/components/icons/IconHeart.js
+++ src/components/icons/IconHeart.js
@@ -1,4 +1,11 @@
+import React, { useState } from "react";
+
 function IconHeart({ size }) {
+  const [liked, setLiked] = useState(false);
+
+  const handleHeartClick = () => {
+    setLiked(!liked);
+  };
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,10 +15,11 @@ function IconHeart({ size }) {
       viewBox="0 0 24 24"
       strokeWidth="1.5"
       stroke="currentColor"
-      fill="none"
+      fill={liked ? "red" : "none"}
       strokeLinecap="round"
       strokeLinejoin="round"
-      //   opacity={track.liked ? "1" : "0.75"}
+      onClick={handleHeartClick}
+      style={{ cursor: "pointer" }}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,12 +1,36 @@
-export default function Favorites() {
+import React from "react";
+import TrackItem from "../components/TrackItem";
+
+export default function Favorites({ likedTracks, toggleLiked }) {
+  function convertMsToTime(duration_ms) {
+    var seconds = Math.floor((duration_ms / 1000) % 60);
+    var minutes = Math.floor((duration_ms / (1000 * 60)) % 60);
+
+    var displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+    var displayMinutes = minutes < 10 ? "0" + minutes : minutes;
+
+    return displayMinutes + ":" + displaySeconds;
+  }
+
   return (
-    <>
-      <h1 className="h-10 my-10 w-full bg-slate-500">
-        This is favorites page with content
-      </h1>
-      <h1 className="h-10 my-10 w-full bg-slate-500">
-        This is favorites page with content
-      </h1>
-    </>
+    <div>
+      <h1>Favorites</h1>
+      {likedTracks && likedTracks.length > 0 ? (
+        likedTracks.map((track) => (
+          <TrackItem
+            key={track.id}
+            cover={track.album.images[0]?.url}
+            artist={track.artists[0].name}
+            title={track.name}
+            type="Songs"
+            duration={convertMsToTime(track.duration_ms)}
+            liked={true}
+            onLike={() => toggleLiked(track.id)}
+          />
+        ))
+      ) : (
+        <p>No liked tracks found</p>
+      )}
+    </div>
   );
 }
