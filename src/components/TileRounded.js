c2f4e943b8bab64874074f38bff88fd6655631af
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-05-05 16:21:05 +0800

    eded

--- src/components/TileRounded.js
+++ src/components/TileRounded.js
@@ -2,7 +2,7 @@ function TileRounded(data) {
     return (
         <li className="w-44 h-[14.5rem] text-left hover-underline">
             <a className="flex flex-col justify-between h-full" href="#artist">
-                <img src={data.src} className="w-full rounded-full object-cover" height="auto" alt={data.name + ` ` + data.type} />
+                <img src={data.src} className="w-full rounded-full object-cover h-44" alt={data.name + ` ` + data.type} />
                 <div className="flex flex-col w-full">
                     <span className="truncate text-base">{data.title}</span>
                     <span className="truncate text-xs text-slate-400">{data.subTitle}</span>
