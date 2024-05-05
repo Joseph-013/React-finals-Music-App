c879d37f7fc8c243bc5e0c4f8c0dca7e727b3abc
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-05-05 17:08:04 +0800

    add to playlist button

--- src/components/TrackItem.js
+++ src/components/TrackItem.js
@@ -1,9 +1,11 @@
 import visualizerGif from './icons/visualizerGif.gif';
 import { useState } from 'react';
 import IconPlayFilled from './icons/IconPlayFilled';
+import IconPlaylistAdd from './icons/IconPlaylistAdd';
 
 function TrackItem(track) {
-    const [hovered, setHovered] = useState(false);
+    const [trackHovered, setTrackHovered] = useState(false);
+    // const []
     let widthCount = 0;
     let width;
     if (track.duration) ++widthCount;
@@ -16,13 +18,13 @@ function TrackItem(track) {
 
     return (
         <div className={`w-full h-full flex items-center justify-between text-left space-x-2 cursor-pointer hover:bg-cyan-700 p-2 -mx-2 rounded-lg ` + (!track.playing || 'border border-cyan-600')}
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
+            onMouseEnter={() => setTrackHovered(true)}
+            onMouseLeave={() => setTrackHovered(false)}
         >
             <div className={`h-full flex items-center space-x-3 ` + width}>
                 <div className="relative w-fit">
                     {(!track.playing) || <img src={visualizerGif} alt='Play' className='opacity-40 absolute' />}
-                    <IconPlayFilled size="25" className={`absolute justify-center items-center h-full w-full p-2 bg-gray-900 opacity-60 ` + (hovered && !track.playing && !track.playerComponent ? `flex` : `hidden`)} />
+                    <IconPlayFilled size="25" className={`absolute justify-center items-center h-full w-full p-2 bg-gray-900 opacity-60 ` + (trackHovered && !track.playing && !track.playerComponent ? `flex` : `hidden`)} />
                     <img src={track.cover} alt={track.title} className="size-12" />
                 </div>
                 <div className="flex-1 flex flex-col truncate font-poppins w-fit">
@@ -31,7 +33,7 @@ function TrackItem(track) {
                 </div>
             </div>
             {(!track.genre) || <span className="h-full w-fit flex items-center text-center max-w-24 text-slate-400 line-clamp-2">{track.genre}</span>}
-            {(!track.duration) || <span className="h-full w-fit flex items-center text-slate-400">{track.duration}</span>}
+            {(trackHovered) ? <button className='size-10 flex items-center justify-center rounded-full hover:bg-cyan-600'><IconPlaylistAdd size="25" /></button> : (!track.duration) || <span className="h-full w-fit flex items-center text-slate-400">{track.duration}</span>}
         </div>
     )
 }
---
+++ src/components/icons/IconPlaylistAdd.js
@@ -0,0 +1,14 @@
+function IconPlaylistAdd({ size, className }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" className={`icon icon-tabler icon-tabler-playlist-add ${className}`} width={size} height={size} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M19 8h-14" />
+            <path d="M5 12h9" />
+            <path d="M11 16h-6" />
+            <path d="M15 16h6" />
+            <path d="M18 13v6" />
+        </svg>
+    )
+}
+
+export default IconPlaylistAdd
