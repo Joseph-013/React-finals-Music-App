51a4437e68ef923006a48139547d07f92442c927
SeraphLaur <laurence.arcila@gmail.com>
2024-05-09 21:54:32 +0800

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -79,17 +79,9 @@ export default function Home({
         albums: [],
         artists: [],
         tracks: [],
-    };
-    const [musics, setMusic] = useState(initialMusicsState);
-
-    function getRandomSearch() {
-        const characters = "abcdefghijklmnopqrstuvwxyz";
-        const randomCharacter = characters.charAt(
-            Math.floor(Math.random() * characters.length)
-        );
-
-        return randomCharacter;
+      };
     }
+  }
 
   // const handleItemClick = (item, category) => {
   //   item.status = true;
@@ -108,99 +100,39 @@ export default function Home({
     var seconds = Math.floor((duration_ms / 1000) % 60);
     var minutes = Math.floor((duration_ms / (1000 * 60)) % 60);
 
-    async function search() {
-        const randomSearch = getRandomSearch();
-        console.log("Searching for.. " + randomSearch);
+    var displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+    var displayMinutes = minutes < 10 ? "0" + minutes : minutes;
 
-        try {
-            const response = await fetch(
-                `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-                    randomSearch
-                )}&type=album,artist,track&limit=50`,
-                {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: "Bearer " + accessToken,
-                    },
-                }
-            );
+    return displayMinutes + ":" + displaySeconds;
+  }
 
-            if (!response.ok) {
-                throw new Error("Failed to fetch data from Spotify API");
-            }
+  return (
+    <div className="w-full space-y-10">
+      <TileGridHorizontal title="Recommended Albums">
+        {musics.albums &&
+          musics.albums.map((music) => (
+            <TileSquared
+              key={music.id}
+              src={music.images[0]?.url}
+              type="Album"
+              title={music.name}
+              subTitle={music.artists[0].name}
+            />
+          ))}
+      </TileGridHorizontal>
 
-            const data = await response.json();
-            console.log("Albums:", data.albums.items);
-            console.log("Artists:", data.artists.items);
-            console.log("Tracks:", data.tracks.items);
-            return {
-                albums: data.albums.items,
-                artists: data.artists.items,
-                tracks: data.tracks.items,
-            };
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            return {
-                albums: [],
-                artists: [],
-                tracks: [],
-            };
-        }
-    }
+      <TileGridHorizontal title="Recommended Artists">
+        {musics.artists &&
+          musics.artists.map((music) => (
+            <TileRounded
+              key={music.id}
+              src={music.images[0]?.url}
+              type="Artist"
+              title={music.name}
+            />
+          ))}
+      </TileGridHorizontal>
 
-    function convertMsToTime(duration_ms) {
-        var seconds = Math.floor((duration_ms / 1000) % 60);
-        var minutes = Math.floor((duration_ms / (1000 * 60)) % 60);
-
-        var displaySeconds = seconds < 10 ? "0" + seconds : seconds;
-        var displayMinutes = minutes < 10 ? "0" + minutes : minutes;
-
-        return displayMinutes + ":" + displaySeconds;
-    }
-
-    return (
-        <div className="w-full space-y-10">
-            <TileGridHorizontal title="Recommended Albums">
-                {musics.albums &&
-                    musics.albums.map((music) => (
-                        <TileSquared
-                            key={music.id}
-                            src={music.images[0]?.url}
-                            type="Album"
-                            title={music.name}
-                            subTitle={music.artists[0].name}
-                        />
-                    ))}
-            </TileGridHorizontal>
-
-            <TileGridHorizontal title="Recommended Artists">
-                {musics.artists &&
-                    musics.artists.map((music) => (
-                        <TileRounded
-                            key={music.id}
-                            src={music.images[0]?.url}
-                            type="Artist"
-                            title={music.name}
-                        />
-                    ))}
-            </TileGridHorizontal>
-
-            <ListGridVertical title="Recommended Songs" >
-                {musics.tracks &&
-                    musics.tracks.map((music) => (
-                        <TrackItem
-                            key={music.id}
-                            cover={music.album.images[0]?.url}
-                            artist={music.artists[0].name}
-                            title={music.name}
-                            type="Songs"
-                            duration={convertMsToTime(music.duration_ms)}
-                        />
-                    ))}
-            </ListGridVertical>
-        </div>
-    );
       <ListGridVertical title="Recommended Songs">
         {musics.tracks &&
           musics.tracks.map((music) => (
