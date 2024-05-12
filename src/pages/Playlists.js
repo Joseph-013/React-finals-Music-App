9e1cb4dbb5c79056124b251705ae0a1a177ac130
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-05-12 14:32:47 +0800

    playlists

--- src/pages/Playlists.js
+++ src/pages/Playlists.js
@@ -1,48 +1,29 @@
+import PlaylistItem from "../components/PlaylistItem";
+import SectionContainer from "../components/SectionContainer";
+
 export default function Playlists() {
     return (
-        <>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-            <h1 className="h-10 my-10 w-full bg-slate-500">This is playlists page with content</h1>
-        </>
+        <div className="w-full flex flex-col">
+
+            <SectionContainer title="Playlists">
+                <div className="w-full grid grid-flow-row grid-cols-2 sm:grid-cols-3 min-[867px]:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-7">
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                    <PlaylistItem />
+                </div>
+            </SectionContainer>
+
+
+        </div>
     );
 }
