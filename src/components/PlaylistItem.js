7ea4b28cd069cdca7beb3911a3c66b3e8023b114
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-05-12 14:42:26 +0800

    Update PlaylistItem.js

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -2,7 +2,7 @@ function PlaylistItem(playlist) {
     return (
         <div className="gap-y-2 w-48 flex flex-col justify-center text-left hover:underline hover:cursor-pointer hover:bg-slate-700 px-3 py-4">
             <div className="size-40 rounded-md grid grid-rows-2 mx-auto grid-cols-2">
-                {/* Adjust nalang to 1 if yung laman is less than 4 */}
+                {/* Adjust nalang to 1 img if yung count is less than 4 */}
                 <img src="https://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png" alt="albumName" className="object-cover" />
                 <img src="https://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png" alt="albumName" className="object-cover" />
                 <img src="https://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png" alt="albumName" className="object-cover" />
