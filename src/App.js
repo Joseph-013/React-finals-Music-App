3dec91e4eed7a286b989b5781c7648981d8094d1
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-05-16 22:11:22 +0800

    last update

--- src/App.js
+++ src/App.js
@@ -18,157 +18,157 @@ import Overlay from "./components/Overlay";
 const CLIENT_ID = "1b6704c6a4c340899a3d4f5f0e407358";
 const CLIENT_SECRET = "48dfec19fefc4ae48dfdfd5e48cdaa40";
 const tmpPlaylists = {
-  "test playlist 1": ["song 1", "song 2"],
-  "test playlist 2": ["song 3", "song 4"],
+    "test playlist 1": ["song 1", "song 2"],
+    "test playlist 2": ["song 3", "song 4"],
 };
 
 function App() {
-  const [accessToken, setAccessToken] = useState("");
-  const [recent, setRecent] = useState({ albums: [], artists: [], tracks: [] });
-  const [data, setData] = useState({
-    albums: [],
-    artists: [],
-    tracks: [],
-  });
+    const [accessToken, setAccessToken] = useState("");
+    const [recent, setRecent] = useState({ albums: [], artists: [], tracks: [] });
+    const [data, setData] = useState({
+        albums: [],
+        artists: [],
+        tracks: [],
+    });
 
-  const [playlists, setPlaylists] = useState(tmpPlaylists);
-  const [likedTracks, setLikedTracks] = useState([]);
-  const [loading, setLoading] = useState(true); // Loading state
-  const [overlay, setOverlay] = useState(false);
+    const [playlists, setPlaylists] = useState(tmpPlaylists);
+    const [likedTracks, setLikedTracks] = useState([]);
+    const [loading, setLoading] = useState(true); // Loading state
+    const [overlay, setOverlay] = useState(false);
 
-  useEffect(() => {
-    var authParameters = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body:
-        "grant_type=client_credentials&client_id=" +
-        CLIENT_ID +
-        "&client_secret=" +
-        CLIENT_SECRET,
+    useEffect(() => {
+        var authParameters = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body:
+                "grant_type=client_credentials&client_id=" +
+                CLIENT_ID +
+                "&client_secret=" +
+                CLIENT_SECRET,
+        };
+
+        fetch("https://accounts.spotify.com/api/token", authParameters)
+            .then((res) => res.json())
+            .then((data) => {
+                setAccessToken(data.access_token);
+                setLoading(false); // Update loading state once access token is fetched
+            });
+    }, []);
+
+    const removeTrack = (trackId) => {
+        setLikedTracks((prevLikedTracks) => {
+            const updatedLikedTracks = Object.fromEntries(
+                Object.entries(prevLikedTracks).filter(([id, track]) => id !== trackId)
+            );
+            return updatedLikedTracks;
+        });
     };
 
-    fetch("https://accounts.spotify.com/api/token", authParameters)
-      .then((res) => res.json())
-      .then((data) => {
-        setAccessToken(data.access_token);
-        setLoading(false); // Update loading state once access token is fetched
-      });
-  }, []);
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-  const removeTrack = (trackId) => {
-    setLikedTracks((prevLikedTracks) => {
-      const updatedLikedTracks = Object.fromEntries(
-        Object.entries(prevLikedTracks).filter(([id, track]) => id !== trackId)
-      );
-      return updatedLikedTracks;
-    });
-  };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  const toggleLiked = (trackId) => {
-    setLikedTracks((prevState) => {
-      const updatedLikedTracks = { ...prevState };
-      if (updatedLikedTracks[trackId]) {
-        updatedLikedTracks[trackId].liked = !updatedLikedTracks[trackId].liked;
-      } else {
-        const foundTrack = data.tracks.find((track) => track.id === trackId);
-        if (foundTrack) {
-          foundTrack.liked = true;
-          updatedLikedTracks[trackId] = foundTrack;
-        }
-      }
-      return updatedLikedTracks;
-    });
-  };
-
-  function addSongToPlaylist(playlistName, trackID) {
-    setPlaylists(() => {
-      var updatedPlaylist = [...playlists[playlistName], trackID];
-
-      console.log("Song is added");
-      return { ...playlists, [playlistName]: updatedPlaylist };
-    });
-  }
-
-  return (
-    <PlaylistContext.Provider
-      value={{ playlists, setPlaylists, setOverlay, addSongToPlaylist }}
-    >
-      <div className="w-screen h-screen text-[#d9d9d9] bg-[#121C21] tracking-wide">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route
-                path="testpage"
-                element={
-                  <TestPage playlists={playlists}>
-                    <NewPlaylist setPlaylists={setPlaylists} />
-                  </TestPage>
+    const toggleLiked = (trackId) => {
+        setLikedTracks((prevState) => {
+            const updatedLikedTracks = { ...prevState };
+            if (updatedLikedTracks[trackId]) {
+                updatedLikedTracks[trackId].liked = !updatedLikedTracks[trackId].liked;
+            } else {
+                const foundTrack = data.tracks.find((track) => track.id === trackId);
+                if (foundTrack) {
+                    foundTrack.liked = true;
+                    updatedLikedTracks[trackId] = foundTrack;
                 }
-              />
-              <Route
-                index
-                element={
-                  <Home
-                    accessToken={accessToken}
-                    setRecent={setRecent}
-                    likedTracks={likedTracks}
-                    data={data}
-                    setData={setData}
-                    setLikedTracks={setLikedTracks}
-                    toggleLiked={toggleLiked}
-                  />
-                }
-              />
-              <Route
-                path="discover"
-                element={
-                  <Discover
-                    accessToken={accessToken}
-                    setRecent={setRecent}
-                    likedTracks={likedTracks}
-                    data={data}
-                    setData={setData}
-                    setLikedTracks={setLikedTracks}
-                    toggleLiked={toggleLiked}
-                  />
-                }
-              />
-              <Route
-                path="trending"
-                element={<Trending accessToken={accessToken} />}
-              />
-              <Route path="recent" element={<Recent recent={recent} />} />
-              <Route
-                path="playlists"
-                element={<Playlists playlists={playlists} />}
-              />
-              <Route
-                path="favorites"
-                element={
-                  <Favorites
-                    likedTracks={likedTracks}
-                    toggleLiked={toggleLiked}
-                    removeTrack={removeTrack}
-                  />
-                }
-              />
-            </Route>
-            <Route path="*" element={<NoPage />} />
-          </Routes>
-        </BrowserRouter>
-        <Overlay
-          overlay={overlay}
-          setOverlay={setOverlay}
-          setPlaylists={setPlaylists}
-        />
-      </div>
-    </PlaylistContext.Provider>
-  );
+            }
+            return updatedLikedTracks;
+        });
+    };
+
+    function addSongToPlaylist(playlistName, trackID) {
+        setPlaylists(() => {
+            var updatedPlaylist = [...playlists[playlistName], trackID];
+
+            console.log("Song is added");
+            return { ...playlists, [playlistName]: updatedPlaylist };
+        });
+    }
+
+    return (
+        <PlaylistContext.Provider
+            value={{ playlists, setPlaylists, setOverlay, addSongToPlaylist }}
+        >
+            <div className="w-screen h-screen text-[#d9d9d9] bg-[#121C21] tracking-wide">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout />}>
+                            <Route
+                                path="testpage"
+                                element={
+                                    <TestPage playlists={playlists}>
+                                        <NewPlaylist setPlaylists={setPlaylists} />
+                                    </TestPage>
+                                }
+                            />
+                            <Route
+                                index
+                                element={
+                                    <Home
+                                        accessToken={accessToken}
+                                        setRecent={setRecent}
+                                        likedTracks={likedTracks}
+                                        data={data}
+                                        setData={setData}
+                                        setLikedTracks={setLikedTracks}
+                                        toggleLiked={toggleLiked}
+                                    />
+                                }
+                            />
+                            <Route
+                                path="discover"
+                                element={
+                                    <Discover
+                                        accessToken={accessToken}
+                                        setRecent={setRecent}
+                                        likedTracks={likedTracks}
+                                        data={data}
+                                        setData={setData}
+                                        setLikedTracks={setLikedTracks}
+                                        toggleLiked={toggleLiked}
+                                    />
+                                }
+                            />
+                            <Route
+                                path="trending"
+                                element={<Trending accessToken={accessToken} />}
+                            />
+                            <Route path="recent" element={<Recent recent={recent} />} />
+                            <Route
+                                path="playlists"
+                                element={<Playlists playlists={playlists} />}
+                            />
+                            <Route
+                                path="favorites"
+                                element={
+                                    <Favorites
+                                        likedTracks={likedTracks}
+                                        toggleLiked={toggleLiked}
+                                        removeTrack={removeTrack}
+                                    />
+                                }
+                            />
+                        </Route>
+                        <Route path="*" element={<NoPage />} />
+                    </Routes>
+                </BrowserRouter>
+                <Overlay
+                    overlay={overlay}
+                    setOverlay={setOverlay}
+                    setPlaylists={setPlaylists}
+                />
+            </div>
+        </PlaylistContext.Provider>
+    );
 }
 export default App;
--- src/pages/Playlists.js
+++ src/pages/Playlists.js
@@ -1,20 +1,83 @@
 import PlaylistItem from "../components/PlaylistItem";
 import SectionContainer from "../components/SectionContainer";
+import TrackItem from "../components/TrackItem";
 
 export default function Playlists({ playlists }) {
-  return (
-    <div className="w-full flex flex-col">
-      <SectionContainer title="Playlists">
-        <div className="w-full grid grid-flow-row grid-cols-2 sm:grid-cols-3 min-[867px]:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-7">
-          {Object.entries(playlists).map(([playlistName, songs]) => (
-            <PlaylistItem
-              key={playlistName}
-              title={playlistName}
-              count={songs.length}
-            />
-          ))}
+    return (
+        <div className="w-full flex flex-col">
+            <SectionContainer title="Playlists">
+                <div className="w-full grid grid-flow-row grid-cols-2 sm:grid-cols-3 min-[867px]:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-7">
+                    {Object.entries(playlists).map(([playlistName, songs]) => (
+                        <PlaylistItem
+                            key={playlistName}
+                            title={playlistName}
+                            count={songs.length}
+                        />
+                    ))}
+                </div>
+            </SectionContainer>
+            <ViewPlaylist />
         </div>
-      </SectionContainer>
-    </div>
-  );
+    );
+
+    function ViewPlaylist() {
+        // <TrackItem
+        //     className="w-[28rem]"
+        //     cover="https://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png"
+        //     title="Red"
+        //     album="But"
+        //     artist="Regine Velasquez"
+        //     duration="3:55"
+        //     playlists={playlists}
+        // />
+        const testItem = (
+            < TrackItem
+                className="w-[28rem]"
+                cover="https://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png"
+                title="Red"
+                album="But"
+                artist="Regine Velasquez"
+                duration="3:55"
+                playlists={playlists}
+            />
+        );
+
+        return (
+            <div className={`absolute border-none top-0 left-0 flex items-center justify-center border w-full h-full z-[10000] overflow-hidden`}>
+                {/* <div className="absolute inset-0 bg-black opacity-50 hover:cursor-pointer" onClick={() => setOverlay(false)}></div> */}
+                <div className="absolute inset-0 bg-black opacity-50 hover:cursor-pointer" />
+
+                <div className="bg-[#203038] p-3 m-3 w-full max-w-[35rem] rounded-lg z-[10001] flex flex-col max-h-screen">
+                    <div className="flex flex-row justify-between w-full pl-3">
+                        <div className="h-full my-auto flex flex-col text-left flex-1 space-y-3">
+                            <span className="text-xl font-bold line-clamp-3">
+                                Playlist Name
+                            </span>
+                            <span className="text-gray-500"># of tracks</span>
+                        </div>
+
+                        <div className="size-40 rounded-md grid grid-rows-2 grid-cols-2">
+                            <img src="https://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png" alt="albumName" className="object-cover" />
+                            <img src="https://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png" alt="albumName" className="object-cover" />
+                            <img src="https://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png" alt="albumName" className="object-cover" />
+                            <img src="https://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png" alt="albumName" className="object-cover" />
+                        </div>
+
+                    </div>
+                    <div className="mx-5 my-2" style={{ borderBottom: "solid 1px gray" }} />
+                    <div className="w-full flex flex-col items-center space-y-2 max-h-[20rem] overflow-y-auto" style={{ overscrollBehaviorY: "contain" }}>
+                        {testItem}
+                        {testItem}
+                        {testItem}
+                        {testItem}
+                        {testItem}
+                        {testItem}
+                        {testItem}
+                        {testItem}
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
 }
