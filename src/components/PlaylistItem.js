64786fe4841b932d54bf2d474587c0fbc2b5b73a
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-05-19 23:15:59 +0800

    Update PlaylistItem.js

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -3,7 +3,7 @@ function PlaylistItem(playlist) {
         <div className="gap-y-2 w-44 flex flex-col justify-center text-left hover:underline hover:cursor-pointer hover:bg-slate-700 rounded-lg px-3 py-4">
             <div className="size-36 rounded-md">
                 {/* Adjust nalang to 1 img if yung count is less than 4 */}
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-vinyl" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-vinyl" viewBox="0 0 24 24" strokeWidth="0.7" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M16 3.937a9 9 0 1 0 5 8.063" />
                     <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
